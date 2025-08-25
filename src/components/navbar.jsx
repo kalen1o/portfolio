6c0ffec18fd9cefb0aa71afaf527f97f1fc30d76
Quang Tran <ngungonngon@gmail.com>
2025-08-25 13:57:48 +0700

    Remove logo section from Navbar component and simplify social links layout

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -77,20 +77,9 @@ const Navbar = () => {
                     <NavLink link={link} key={link.title} />
                 ))}
             </div>
-            {/* LOGO */}
-            <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
-                <Link
-                    href="/"
-                    className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
-                >
-                    <span className="text-white mr-1">Lama</span>
-                    <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">
-                        .dev
-                    </span>
-                </Link>
-            </div>
+
             {/* SOCIAL */}
-            <div className="hidden md:flex gap-4 w-1/3">
+            <div className="hidden md:flex gap-4">
                 <Link href="https://github.com/kalen1o">
                     <Image src={getImagePath("/github.png")} alt="" width={24} height={24} />
                 </Link>
