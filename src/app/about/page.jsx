a4f29634b451fd985f75c41a9d1c73c20413d783
Quang Tran <ngungonngon@gmail.com>
2025-08-26 22:35:07 +0700

    Refactor AboutPage layout by adjusting padding classes and correcting background color for line circle element.

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -26,7 +26,7 @@ const AboutPage = () => {
             {/* CONTAINER */}
             <div className="h-full overflow-scroll lg:flex" ref={containerRef}>
                 {/* TEXT CONTAINER */}
-                <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64 lg:w-2/3 lg:pr-0 xl:w-1/2">
+                <div className="p-4 sm:p-8 md:p-12 lg:p-20 flex flex-col gap-24 md:gap-32 lg:gap-48 lg:w-2/3 lg:pr-0">
                     {/* BIOGRAPHY CONTAINER */}
                     <div className="flex flex-col gap-12 justify-center">
                         {/* BIOGRAPHY IMAGE */}
@@ -190,7 +190,7 @@ const AboutPage = () => {
                                     {/* LINE */}
                                     <div className="w-1 h-full bg-gray-600 rounded relative">
                                         {/* LINE CIRCLE */}
-                                        <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                                        <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-red-400 -left-2"></div>
                                     </div>
                                 </div>
                                 {/* RIGHT */}
